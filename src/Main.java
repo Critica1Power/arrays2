fbe2cdebfedd3e61fc3762c8c21bd8c772d16040
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-18 18:23:17 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] array1 = generateRandomArray();
         // Задание 1
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        int summa = 0;
+        for (int i = 0; i < array1.length; i++) {
+            summa += array1[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + summa + " рублей.");
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
